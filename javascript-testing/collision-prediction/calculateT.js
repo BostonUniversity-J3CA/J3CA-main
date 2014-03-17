11eca8add63cb5068c179e5fff7364e1059453be
Joseph Stone <jestone@bu.edu>
2014-03-17 07:32:01 -0600

    Here is a big update for the collision prediction. I've gone through 3 different models for detecting the collision with limited success. My next idea is to extrapolate on the ray detection idea: I know that in 2D it works flawlessly with a static circle. Now, I'm going to have the circle move in a linear direction. The mathematics was very difficult, so I used MATLAB to create the equation. Now I just need to implement this equation ( which is in calculateT.js )

---
+++ javascript-testing/collision-prediction/calculateT.js
@@ -0,0 +1,35 @@
+﻿function calculateT(bx,by,bz,aircraft,m,dx,dy,dz,R){
+    var t = bx*dx + by*dy + bz*dz - bx*m - by*m - bz*m - dx*aircraft.prevLon - dy*aircraft.prevLat - dz*aircraft.prevAlt + m*aircraft.prevLon 
+	+ m*aircraft.prevLat + m*aircraft.prevAlt + Math.sqrt(Math.pow(R,2)*Math.pow(dx,2) - 2*Math.pow(R,2)*dx*m + Math.pow(R,2)*Math.pow(dy,2) 
+        - 2*Math.pow(R,2)*dy*m + Math.pow(R,2)*Math.pow(dz,2) - 2*Math.pow(R,2)*dz*m + Math.pow(R,2)*Math.pow(m,2) + Math.pow(R,2)*Math.pow(m,2) 
+        + Math.pow(R,2)*Math.pow(m,2) - Math.pow(bx,2)*Math.pow(dy,2) + 2*Math.pow(bx,2)*dy*m - Math.pow(bx,2)*Math.pow(dz,2) 
+        + 2*Math.pow(bx,2)*dz*m - Math.pow(bx,2)*Math.pow(m,2) - Math.pow(bx,2)*Math.pow(m,2) + 2*bx*by*dx*dy - 2*bx*by*dx*m - 2*bx*by*dy*m 
+        + 2*bx*by*m*m + 2*bx*bz*dx*dz - 2*bx*bz*dx*m - 2*bx*bz*dz*m + 2*bx*bz*m*m - 2*bx*dx*dy*aircraft.prevLat - 2*bx*dx*dz*aircraft.prevAlt 
+        + 2*bx*dx*m*aircraft.prevLat + 2*bx*dx*m*aircraft.prevAlt + 2*bx*Math.pow(dy,2)*aircraft.prevLon + 2*bx*dy*m*aircraft.prevLat 
+        - 4*bx*dy*m*aircraft.prevLon + 2*bx*Math.pow(dz,2)*aircraft.prevLon + 2*bx*dz*m*aircraft.prevAlt - 4*bx*dz*m*aircraft.prevLon 
+        - 2*bx*m*m*aircraft.prevLat - 2*bx*m*m*aircraft.prevAlt + 2*bx*Math.pow(m,2)*aircraft.prevLon + 2*bx*Math.pow(m,2)*aircraft.prevLon 
+        - Math.pow(by,2)*Math.pow(dx,2) + 2*Math.pow(by,2)*dx*m - Math.pow(by,2)*Math.pow(dz,2) + 2*Math.pow(by,2)*dz*m 
+        - Math.pow(by,2)*Math.pow(m,2) - Math.pow(by,2)*Math.pow(m,2) + 2*by*bz*dy*dz - 2*by*bz*dy*m - 2*by*bz*dz*m + 2*by*bz*m*m 
+        + 2*by*Math.pow(dx,2)*aircraft.prevLat - 2*by*dx*dy*aircraft.prevLon - 4*by*dx*m*aircraft.prevLat + 2*by*dx*m*aircraft.prevLon 
+        - 2*by*dy*dz*aircraft.prevAlt + 2*by*dy*m*aircraft.prevLon + 2*by*dy*m*aircraft.prevAlt + 2*by*Math.pow(dz,2)*aircraft.prevLat 
+        + 2*by*dz*m*aircraft.prevAlt - 4*by*dz*m*aircraft.prevLat + 2*by*Math.pow(m,2)*aircraft.prevLat - 2*by*m*m*aircraft.prevLon 
+        - 2*by*m*m*aircraft.prevAlt + 2*by*Math.pow(m,2)*aircraft.prevLat - Math.pow(bz,2)*Math.pow(dx,2) + 2*Math.pow(bz,2)*dx*m 
+        - Math.pow(bz,2)*Math.pow(dy,2) + 2*Math.pow(bz,2)*dy*m - Math.pow(bz,2)*Math.pow(m,2) - Math.pow(bz,2)*Math.pow(m,2) 
+        + 2*bz*Math.pow(dx,2)*aircraft.prevAlt - 2*bz*dx*dz*aircraft.prevLon - 4*bz*dx*m*aircraft.prevAlt + 2*bz*dx*m*aircraft.prevLon 
+        + 2*bz*Math.pow(dy,2)*aircraft.prevAlt - 2*bz*dy*dz*aircraft.prevLat - 4*bz*dy*m*aircraft.prevAlt + 2*bz*dy*m*aircraft.prevLat 
+        + 2*bz*dz*m*aircraft.prevLon + 2*bz*dz*m*aircraft.prevLat + 2*bz*Math.pow(m,2)*aircraft.prevAlt - 2*bz*m*m*aircraft.prevLon 
+        + 2*bz*Math.pow(m,2)*aircraft.prevAlt - 2*bz*m*m*aircraft.prevLat - Math.pow(dx,2)*Math.pow(aircraft.prevLat,2) 
+        - Math.pow(dx,2)*Math.pow(aircraft.prevAlt,2) + 2*dx*dy*aircraft.prevLon*aircraft.prevLat + 2*dx*dz*aircraft.prevLon*aircraft.prevAlt 
+        + 2*dx*m*Math.pow(aircraft.prevLat,2) + 2*dx*m*Math.pow(aircraft.prevAlt,2) - 2*dx*m*aircraft.prevLon*aircraft.prevLat 
+        - 2*dx*m*aircraft.prevLon*aircraft.prevAlt - Math.pow(dy,2)*Math.pow(aircraft.prevLon,2) - Math.pow(dy,2)*Math.pow(aircraft.prevAlt,2) 
+        + 2*dy*dz*aircraft.prevLat*aircraft.prevAlt - 2*dy*m*aircraft.prevLon*aircraft.prevLat + 2*dy*m*Math.pow(aircraft.prevLon,2) 
+        + 2*dy*m*Math.pow(aircraft.prevAlt,2) - 2*dy*m*aircraft.prevLat*aircraft.prevAlt - Math.pow(dz,2)*Math.pow(aircraft.prevLon,2) 
+        - Math.pow(dz,2)*Math.pow(aircraft.prevLat,2) - 2*dz*m*aircraft.prevLon*aircraft.prevAlt - 2*dz*m*aircraft.prevLat*aircraft.prevAlt 
+        + 2*dz*m*Math.pow(aircraft.prevLon,2) + 2*dz*m*Math.pow(aircraft.prevLat,2) - Math.pow(m,2)*Math.pow(aircraft.prevLat,2) 
+        - Math.pow(m,2)*Math.pow(aircraft.prevAlt,2) + 2*m*m*aircraft.prevLon*aircraft.prevLat + 2*m*m*aircraft.prevLon*aircraft.prevAlt 
+        - Math.pow(m,2)*Math.pow(aircraft.prevLon,2) - Math.pow(m,2)*Math.pow(aircraft.prevAlt,2) + 2*m*m*aircraft.prevLat*aircraft.prevAlt 
+        - Math.pow(m,2)*Math.pow(aircraft.prevLon,2) - Math.pow(m,2)*Math.pow(aircraft.prevLat,2))) / (Math.pow(dx,2) 
+        - 2*dx*m + Math.pow(dy,2) - 2*dy*m + Math.pow(dz,2) - 2*dz*m + Math.pow(m,2) + Math.pow(m,2) + Math.pow(m,2);
+													     
+        return t;
+}
--- javascript-testing/collision-prediction/prediction.js
+++ javascript-testing/collision-prediction/prediction.js
@@ -10,10 +10,9 @@ var canvasHeight = 0;
 // Animation variables
 var playAnimation = true;
 // Constants
-var NUM_OBSTACLES = 20;
-var DETECTION_DISTANCE  = 600;
-var MIN_TIME_DIFF       = 5;
-var IGNORE_TIME         = -0.5;
+var NUM_OBSTACLES = 1;
+var MAX_CLOSENESS = 3; // The difference in seconds between the s and t constants in the collision formula. If s = t, then the obstacle and our
+// aircraft will be at the same exact point in space at that time.
 function Aircraft(){
     var self     = this;
     var radius   = 2*zoom; // The length of the aircraft is 1 meter but I'm applying a factor of safety of 2
@@ -32,6 +31,11 @@ function Aircraft(){
 	y : 0, 
 	z : 0
     }
+    var prevPosition = {
+	x : 0,
+	y : 0,
+	z : 0
+    }
     this.color   = "#00f";
     this.setVelocity = function(x,y,z){
 	velocity.x = x;
@@ -44,6 +48,9 @@ function Aircraft(){
 	position.z = z;
     }
     this.move        = function(){
+	prevPosition.x = position.x;
+	prevPosition.y = position.y;
+	prevPosition.z = position.z;
 	position.x += velocity.x/60; // Velocity is in pixels per second, dividing by 60 will give pixels per frame
 	position.y += velocity.y/60;
 	position.z += velocity.z/60;
@@ -80,6 +87,9 @@ function Aircraft(){
     this.getVelocity = function(){
 	return velocity;
     }
+    this.getPrevPosition = function(){
+	return prevPosition;
+    }
     return self;
 }
 function rand(min,max){
@@ -111,39 +121,201 @@ function update(){
     return;
 }
 function willCollide(obj1,obj2){
-    var R  = (obj1.getRadius()+obj2.getRadius()); // Assume our aircraft is a point, and give the other aircraft a radius of our aircraft radius plus
-    // the obstacle's aircraft radius
     var vel1 = obj1.getVelocity();
     var vel2 = obj2.getVelocity();
     var pos1 = obj1.getPosition();
     var pos2 = obj2.getPosition();
-    
-    var dv   = [vel1.x-vel2.x,vel1.y-vel2.y,vel1.z-vel2.z];
-    var dp   = [pos1.x-pos2.x,pos1.y-pos2.y,pos1.z-pos2.z];
+    var t    = 0;
+    var s    = 0;
+    var code1= 0;
+    var code2= 0;
 
-    var times= [];
-    for ( var i = 0, n = dv.length; i < n; i++ ){
-	if ( dv[i] == 0 ){
-	    times[i] = 0;
+    if ( vel1.x != 0 ){
+	code1 += 1;
+    }
+    if ( vel1.y != 0 ){
+	code1 += 2;
+    }
+    if ( vel1.z != 0 ){
+	code1 += 4;
+    }
+    if ( vel2.x != 0 ){
+	code2 += 1;
+    }
+    if ( vel2.y != 0 ){
+	code2 += 2;
+    }
+    if ( vel2.z != 0 ){
+	code2 += 4;
+    }
+    if ( code2 == 0 ){
+	// If the obstacle is stationary
+	if ( code1 > 0 ){
+	    if ( code1 % 2 == 1 ){
+		// If the code is odd, then the velocity in x is always not zero
+		t = ( pos2.x - pos1.x ) / vel1.x;
+		s = 0;
+	    }
+	    else if ( code1 == 4 ){
+		t = ( pos2.z - pos1.z ) / vel1.z;
+		s = 0;
+	    }
+	    else {
+		t = ( pos2.y - pos1.y ) / vel1.y;
+		s = 0;
+	    }
 	}
 	else {
-	    times[i] = ( R - dp[i] ) / dv[i];
+	    return false; // In this scenario, neither of the aircraft are moving....so no collision is remotely possible
 	}
     }
-    /*if ( Math.sqrt(times[0]^2+times[1]^2) < DETECTION_DISTANCE ){
-	return true;
-    }*/
-    var timediff = Math.abs(times[0]-times[1]);
-    var dd       = Math.sqrt( Math.pow(dp[0],2) + Math.pow(dp[1],2) );
-    //$("#time_display").html(timediff+"<br/>"+dd);
-    if ( timediff < MIN_TIME_DIFF ){
-	if ( times[0] > IGNORE_TIME && times[1] > IGNORE_TIME ){
-	    if ( dd < DETECTION_DISTANCE ){
-		return true;		    
+    else if ( code1 == 0 ){
+	// If our aircraft is stationary but another aircraft is moving
+	if ( code2 > 0 ){
+	    if ( code2 % 2 == 1 ){
+		// If the code is odd
+		t = 0;
+		s = ( pos1.x - pos2.x ) / vel2.x;
+	    }
+	    else if ( code2 == 4 ){
+		// If only the z velocity is non-zero
+		t = 0;
+		s = ( pos1.z - pos2.z ) / vel2.z;
+	    }
+	    else {
+		// If only the y velocity is non-zero
+		t = 0;
+		s = ( pos1.y - pos2.y ) / vel2.y;
+	    }
+	}
+	else {
+	    return false; // Both aircraft are not moving
+	}
+    }
+    else {
+	if ( code1 == code2 ){
+	    if ( code1 % 2 == 1 ){
+		t = ( pos2.x - pos1.x ) / ( vel1.x - vel2.x );
+	    }
+	    else if ( code1 == 4 ){
+		t = ( pos2.z - pos1.z ) / ( vel1.z - vel2.z );
+	    }
+	    else{
+		t = ( pos2.y - pos1.y ) / ( vel1.y - vel2.y );
+	    }
+	}
+	else {
+	    var ourPos = [0,0]; 
+	    var ourVel = [0,0];
+	    var obsPos = [0,0];
+	    var obsVel = [0,0];
+	    var pos1IsSet = false;
+	    var pos2IsSet = false;
+	    // Check for a velocity in the x-direction
+	    if ( code1 % 2 == 1 ){
+		if ( pos1IsSet == false ){
+		    ourPos[0] = pos1.x;
+		    ourVel[0] = vel1.x;
+		    pos1IsSet = true;
+		}
+		else {
+		    ourPos[1] = pos1.x;
+		    ourVel[1] = vel1.x;
+		}
+	    }
+	    if ( code1 == 4 ){
+		// There's a velocity in the z-direction
+		if ( pos1IsSet == false ){
+		    ourPos[0] = pos1.z;
+		    ourVel[0] = vel1.z;
+		    pos1IsSet = true;
+		}
+		else {
+		    ourPos[1] = pos1.z;
+		    ourVel[1] = vel1.z;
+		}
+	    }
+	    else {
+		// There's a velocity in the y-direction
+		if ( pos1IsSet == false ){
+		    ourPos[0] = pos1.y;
+		    ourVel[0] = vel1.y;
+		    pos1IsSet = true;
+		}
+		else {
+		    ourPos[1] = pos1.y;
+		    ourVel[1] = vel1.y;
+		}
+	    }
+	    // Now compare our aircraft's position and velocity to that of the obstacle to solve for t and s
+	    if ( code2 % 2 == 1 ){
+		// The obstacle has a velocity in the x-direction
+		if ( pos2IsSet == false ){
+		    obsPos[0] = pos2.x;
+		    obsVel[0] = vel2.x;
+		    pos2IsSet = true;
+		}
+		else {
+		    obsPos[1] = pos2.x;
+		    obsVel[1] = vel2.x;
+		}
+	    }
+	    if ( code2 == 4 ){
+		// The obstacle has a velocity in the z-direction
+		if ( pos2IsSet == false ){
+		    obsPos[0] = pos2.z;
+		    obsVel[0] = vel2.z;
+		    pos2IsSet = true;
+		}
+		else {
+		    obsPos[1] = pos2.z;
+		    obsVel[1] = vel2.z;
+		}
+	    }
+	    else {
+		// The obstacle has a velocity in the x-direction
+		if ( pos2IsSet == false ){
+		    obsPos[0] = pos2.y;
+		    obsVel[0] = vel2.y;
+		    pos2IsSet = true;
+		}
+		else {
+		    obsPos[1] = pos2.y;
+		    obsVel[1] = vel2.y;
+		}
+	    }
+	    if ( ourVel[0] != 0 && ourVel[1] != 0 && obsVel[0] != 0 && obsVel[1] != 0 ){
+		t = solveForTWithS(ourPos,ourVel,obsPos,obsVel,obj1.getRadius(),obj2.getRadius());
+		s = ( ourPos[1] - obsPos[1] + ( t * ourVel[1] ) ) / ourVel[1];
+	    }
+	    else if ( obsVel[0] != 0 && obsVel[1] != 0 ){
+		// This function still needs to be written
+		s = ( obsPos[1] - ourPos[1] ) / obsVel[1];
+		if ( ourVel[0] != 0 ){
+		    t = ( obsPos[0] - ourPos[0] + ( s * obsVel[0] ) ) / ( ourVel[0] );
+		}
+		else {
+		    t = ( obsPos[0] - ourPos[0] + ( s * obsVel[0] ) ) / ( ourVel[1] );
+		}
 	    }
 	}
     }
+    $("#time_display").html(t.toFixed(0)+","+s.toFixed(0)+","+(t-s).toFixed(0));
+    if ( Math.abs(t - s) < MAX_CLOSENESS ) {
+	cx.beginPath();
+	cx.arc(t*vel1.x-s*vel2.x,t*vel1.y-s*vel2.y,20,0,Math.PI*2,true);
+	cx.strokeStyle="#0f0";
+	cx.stroke();
+	cx.closePath();
+	return true;
+    }
     return false;
+}
+function solveForTWithS(pos1,pos2,vel1,vel2,radius1,radius2){
+    var t = ( pos2[0]-pos1[0] ) / vel1[1];
+    t    += vel2[0] * ( pos1[1] - pos2[1] ) / Math.pow(vel1[1],2);
+    t    /= 1 - ( vel2[1] * vel2[0] / vel1[1] );
+    return t;
 }
 function toggleAnim(){
     playAnimation = !playAnimation;
