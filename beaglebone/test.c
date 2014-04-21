a82585d3202f796579915862854979554c9931f6
Joseph Stone <jestone@bu.edu>
2014-04-21 17:23:37 -0600

    Made a quick update to the adsb_decode file. It was printing when it shouldn't have been. test.c just sends a test signal to the adsb_decode function and prints the results.

--- beaglebone/test.c
+++ beaglebone/test.c
@@ -3,9 +3,9 @@
 #include "adsb_decode.h"
 
 int main(int argc, char *argv[]){
-  char adsb[] = {"fa 1 2 f8 ee c2 fb 9b 31 86 2 50 d2 95 1 4d 0 e8"};
-  float *pos;
-  pos = adsb_decode(adsb);
-  printf("%f\n",pos[0]);
+  char  adsb[] = {"fa 1 2 f8 ee c2 fb 9b 31 86 2 50 d2 95 1 4d 0 e8"};
+  float pos[3];
+  adsb_decode(adsb,pos);
+  printf("%f\n%f\n%f\n",pos[0],pos[1],pos[2]);
   return 0;
 }
