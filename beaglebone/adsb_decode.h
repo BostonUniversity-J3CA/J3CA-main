fb134cb173792e1bbb88719980ff8964ab0d6869
Joseph Stone <jestone@bu.edu>
2014-04-21 20:12:51 -0600

    Finally got it to work through hard coding values....for some reason it just was not liking my for loops. So it should be working now, I tested the FFFFFFFF value and another random value and checking it online.

--- beaglebone/adsb_decode.h
+++ beaglebone/adsb_decode.h
@@ -5,67 +5,91 @@
 #include <string.h>
 #include <math.h>
 
-float hex_to_int(const char hex[]){
+double hex_to_int(const char hex[]){
   // This function takes a character array representing a hexadecimal number and returns the decimal equivalent
-  int   SIZE= strlen(hex);
-  int   i   = 0;
-  float sum = 0;
-  int   n   = 0;
-  int   expn= 0;
+  int    SIZE= strlen(hex);
+  int    i   = 0;
+  double sum = 0;
+  int    n   = 0;
+  int    expn= 0;
   for ( i = SIZE-1; i > -1; i--, expn++ ){
-    switch (hex[i]){
-    case 'a':
-      n = 10;
+    if ( hex[i] != ' ' ){
+      switch (hex[i]){
+      case 'a':
+	n = 10;
+	break;
+      case 'b':
+	n = 11;
+	break;
+      case 'c':
+	n = 12;
       break;
-    case 'b':
-      n = 11;
-      break;
-    case 'c':
-      n = 12;
-      break;
-    case 'd':
-      n = 13;
-      break;
-    case 'e':
-      n = 14;
-      break;
-    case 'f':
-      n = 15;
-      break;
-    default:
-      n = hex[i]-'0';
+      case 'd':
+	n = 13;
+	break;
+      case 'e':
+	n = 14;
+	break;
+      case 'f':
+	n = 15;
+	break;
+      default:
+	n = hex[i]-'0';
+      }
+      sum += n*pow(16,expn);
     }
-    sum += n*pow(16,expn);
   }
   return sum;
 }
 
-int adsb_decode(const char adsb[], float* obs_pos){
+int adsb_decode(const char adsb[], double* obs_pos){
   // The function takes an adbs string (character array) and returns a pointer to an array of 3 floats defining the 
   // obstacles latitude, longitude, and altitude position in degrees
 
-  int i, n; // For loop variables
-  char hex[9]; // Character array representing the hex for the obstacle's position
-  int index = 0; // The current index in the hex array
-  int o     = 0; // The current index in the obs_pos array
-  int count = 0; // Counts the number of spaces
-  for ( i = 7, n = strlen(adsb); i < n; i++ ){
-    if ( adsb[i] != ' ' ){
-      hex[index] = adsb[i];
-      index++;
-    }
-    else {
-      count++;
-      if ( count == 4 ){
-	index = 0;
-	count = 0;
-	obs_pos[o] = hex_to_int(hex);
-	o++;
-	if ( o == 3 )
-	  break;
-      }
-    }
-  }
+  int  i, n;       // For loop variables
+  char hex[8];     // Character array representing the hex for the obstacle's position
+  int  index  = 0; // The current index in the hex array
+  int  o      = 0; // The current index in the obs_pos array
+  int  count  = 0; // Counts the number of spaces
+  
+  // Latitude
+  hex[0] = adsb[9];
+  hex[1] = adsb[10];
+  hex[2] = adsb[12];
+  hex[3] = adsb[13];
+  hex[4] = adsb[15];
+  hex[5] = adsb[16];
+  hex[6] = adsb[18];
+  hex[7] = adsb[19];
+
+  obs_pos[0] = hex_to_int(hex);
+
+  // Longitude
+
+  hex[0] = adsb[21];
+  hex[1] = adsb[22];
+  hex[2] = adsb[24];
+  hex[3] = adsb[25];
+  hex[4] = adsb[27];
+  hex[5] = adsb[28];
+  hex[6] = adsb[30];
+  hex[7] = adsb[31];
+
+  obs_pos[1] = hex_to_int(hex);
+
+  // Longitude
+
+  hex[0] = adsb[33];
+  hex[1] = adsb[34];
+  hex[2] = adsb[36];
+  hex[3] = adsb[37];
+  hex[4] = adsb[39];
+  hex[5] = adsb[40];
+  hex[6] = adsb[42];
+  hex[7] = adsb[43];
+
+  obs_pos[2] = hex_to_int(hex);
+  
   return 1;
 }
 #endif
--- beaglebone/test.c
+++ beaglebone/test.c
@@ -3,8 +3,8 @@
 #include "adsb_decode.h"
 
 int main(int argc, char *argv[]){
-  char  adsb[] = {"fa 1 2 f8 ee c2 fb 9b 31 86 2 50 d2 95 1 4d 0 e8"};
-  float pos[3];
+  char   adsb[] = {"fa 01 02 0e eb 37 df ff ff ff ff ff ff ff ff 4d 00 e8"};
+  double pos[3];
   adsb_decode(adsb,pos);
   printf("%f\n%f\n%f\n",pos[0],pos[1],pos[2]);
   return 0;
