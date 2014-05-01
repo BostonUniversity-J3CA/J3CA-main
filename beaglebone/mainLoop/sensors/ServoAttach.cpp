2465c50833d6a0857bb285fb69413f8cc806b03a
JochiPochi <jaleman@bu.edu>
2014-05-01 16:49:34 -0400

    Update ServoAttach.cpp

--- beaglebone/mainLoop/sensors/ServoAttach.cpp
+++ beaglebone/mainLoop/sensors/ServoAttach.cpp
@@ -1,8 +1,29 @@
 /*
  * ServoAttach.cpp
- *
  *  Created on: Apr 29, 2014
- *      Author: jaleman
+ *      Author: John R. Aleman, College of Engineering, Boston University
+ *      Notes: This Definition of a class to interface with the
+ *      Analog Devices ADXL345 Digital Accelerometer was created based
+ *      on the work of Derek Molloy.
+ *      Copyright John R. ALeman, College of Engineering, Boston University
+ *
+ * 		Redistribution and use in source and binary forms, with or without modification,
+ * 		are permitted provided that the following conditions are met:
+ * 			1. Redistributions of source code must retain the above copyright
+ *    			notice, this list of conditions and the following disclaimer.
+ * 			2. Redistributions in binary form must reproduce the above copyright
+ *    			notice, this list of conditions and the following disclaimer in the
+ *    			documentation and/or other materials provided with the distribution.
+ *
+ * 		THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * 		INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * 		AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL I
+ * 		BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * 		CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * 		GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * 		HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * 		OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * 		SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <iostream>
