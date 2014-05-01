af6d4a432eb9fe1184260a29074fe562ac17b5c1
JochiPochi <jaleman@bu.edu>
2014-05-01 16:58:49 -0400

    Update main.cpp

--- beaglebone/mainLoop/main.cpp
+++ beaglebone/mainLoop/main.cpp
@@ -1,3 +1,36 @@
+/*
+ * Main Code
+ *
+ *
+ *  Created on: Apr 30, 2014
+ *      Author: John, L, Gillespie, John R. Aleman, Joseph E. Stone, Chirag Rathod, Ari J. Morse
+ *      College of Engineering, Boston University
+ *      Notes: Main code for Pitch stabilization of a fixed wing aircraft and sensor data aquisition as well as
+ *      ground control station communication using the Beaglebone Black.
+ *      Copyright G.R.A.M.S., College of Engineering, Boston University
+ *      jaleman@bu.edu, chiragr314@gmail.com, johngillespie27@yahoo.com, jestone@bu.edu, amorse@bu.edu
+ *
+ * 		Redistribution and use in source and binary forms, with or without modification,
+ * 		are permitted provided that the following conditions are met:
+ * 			1. Redistributions of source code must retain the above copyright
+ *    			notice, this list of conditions and the following disclaimer.
+ * 			2. Redistributions in binary form must reproduce the above copyright
+ *    			notice, this list of conditions and the following disclaimer in the
+ *    			documentation and/or other materials provided with the distribution.
+ *
+ * 		THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * 		INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * 		AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL I
+ * 		BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * 		CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * 		GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * 		HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * 		OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * 		SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+
 #include "control.c"
 #include "output.c"
 #include "complementary.c"
